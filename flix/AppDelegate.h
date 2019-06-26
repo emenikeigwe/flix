840295b864685a640166081970e9476f444f8121
Ezi Godfreey-Igwe <arleneigwe@fb.com>
2019-06-26 11:45:57 -0700

    Initial Commit

---
+++ flix/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  flix
+//
+//  Created by arleneigwe on 6/26/19.
+//  Copyright © 2019 arleneigwe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

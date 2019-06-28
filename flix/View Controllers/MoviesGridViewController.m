0a5327cba65f588309b6a5cc6dfeb75a31c28f4e
Ezi Godfreey-Igwe <arleneigwe@fb.com>
2019-06-28 16:48:05 -0700

    This is the version that still retains the grid tab although it crashes :((

--- flix/View Controllers/MoviesGridViewController.m
+++ flix/View Controllers/MoviesGridViewController.m
@@ -41,7 +41,6 @@
 #warning Incomplete implementation, return the number of sections
     return 0;
 }
-
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
 #warning Incomplete implementation, return the number of rows
     return 0;
